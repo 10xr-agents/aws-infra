60f8a0bb932ba1a1eb2ac9d0fd1ca5e741bd532c
Jaswanth <jaswanth@10xr.co>
2024-09-11 03:26:00 +0530

    adding working directory

--- environments/demo-slim/main.tf
+++ environments/demo-slim/main.tf
@@ -1429,35 +1429,3 @@ resource "aws_iam_policy" "redis_access" {
     ]
   })
 }
-
-data "aws_eks_cluster" "livekit_cluster" {
-  name = "${var.project_name}-eks-cluster"
-}
-
-data "aws_eks_cluster_auth" "livekit_cluster" {
-  name = "${var.project_name}-eks-cluster"
-}
-
-# Kubernetes provider configuration
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.livekit_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.livekit_cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.livekit_cluster.name]
-    command     = "aws"
-  }
-}
-
-# Helm provider configuration
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.livekit_cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.livekit_cluster.certificate_authority[0].data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.livekit_cluster.name]
-      command     = "aws"
-    }
-  }
-}
