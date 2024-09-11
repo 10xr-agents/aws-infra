ae356ef7adb778053ae4fe084093c151b6c9deba
Jaswanth <jaswanth@10xr.co>
2024-09-12 00:08:55 +0530

    adding working directory

--- environments/demo-slim/main.tf
+++ environments/demo-slim/main.tf
@@ -1336,17 +1336,14 @@ data "aws_eks_cluster_auth" "cluster" {
   name = "10xr-demo-eks-cluster"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    load_config_file       = false
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubectl" {
+  kubernetes {
+    config_path = "~/.kube/config"
   }
 }
