47be664df73260583d97978bf8da37939454392b
Jaswanth <jaswanth@10xr.co>
2024-09-13 23:58:53 +0530

    adding working directory

--- python/acm_cert_uploader.py
+++ python/acm_cert_uploader.py
@@ -5,9 +5,21 @@ def lambda_handler(event, context):
     s3_client = boto3.client('s3')
 
     s3_bucket = os.environ['S3_BUCKET']
-    certificate_body = os.environ['CERTIFICATE_BODY']
-    private_key = os.environ['PRIVATE_KEY']
-    certificate_chain = os.environ['CERTIFICATE_CHAIN']
+
+    # Define the file paths relative to the Lambda function's directory
+    cert_path = './cert.pem'
+    key_path = './key.pem'
+    chain_path = './chain.pem'
+
+    # Read the certificate body, private key, and certificate chain from the files
+    with open(cert_path, 'rb') as cert_file:
+        certificate_body = cert_file.read()
+
+    with open(key_path, 'rb') as key_file:
+        private_key = key_file.read()
+
+    with open(chain_path, 'rb') as chain_file:
+        certificate_chain = chain_file.read()
 
     # Upload to S3
     s3_client.put_object(Bucket=s3_bucket, Key='cert.pem', Body=certificate_body)
@@ -18,3 +30,4 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': 'Certificate, key, and chain uploaded successfully'
     }
+
