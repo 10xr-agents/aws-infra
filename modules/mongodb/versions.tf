1e8bdf11c5cd5cc9c5a470f20e0912e33a6e5f58
Jaswanth <jaswanth@10xr.co>
2025-06-30 19:16:05 +0530

    adding working directory

--- modules/mongodb/versions.tf
+++ modules/mongodb/versions.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.99.0"
+      version = ">= 5.95.0"
     }
     template = {
       source  = "hashicorp/template"
