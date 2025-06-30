f323c1633c1f59dfb4108c6916376d05c9670494
Jaswanth <jaswanth@10xr.co>
2025-06-30 20:37:36 +0530

    adding working directory

--- environments/qa/versions.tf
+++ environments/qa/versions.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.1.1"
+      version = ">= 6.1.0"
     }
     template = {
       source  = "hashicorp/template"
