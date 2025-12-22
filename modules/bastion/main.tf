6b318b35bf69cce8acf3b46548812e3246518abe
Jaswanth <jaswanth@10xr.co>
2025-12-22 16:14:04 +0530

    adding n8n service

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -17,8 +17,9 @@ data "aws_ami" "amazon_linux_2023" {
   owners      = ["amazon"]
 
   filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    name = "name"
+    # Use standard AMI with SSM agent pre-installed (exclude minimal)
+    values = ["al2023-ami-2023.*-kernel-*-x86_64"]
   }
 
   filter {
@@ -29,6 +30,11 @@ data "aws_ami" "amazon_linux_2023" {
   filter {
     name   = "root-device-type"
     values = ["ebs"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
